3e6533d23384c08a3dfdcdca3d0164b7a1141525
Anton Storozhev <i.am@tonysparked.com>
2022-12-11 17:29:59 +0300

    go vet fixes & split ci job

--- recipe-service/internal/recipe/storage.go
+++ recipe-service/internal/recipe/storage.go
@@ -6,7 +6,7 @@ type Storage interface {
 	Create(ctx context.Context, recipe Recipe) (string, error)
 	GetById(ctx context.Context, id string) (Recipe, error)
 	GetAll(ctx context.Context, start int, limit int) ([]Recipe, error)
-	GetAllByUser(ctx context.Context, userId string) ([]Recipe, error)
+	GetAllByUser(ctx context.Context, userID string) ([]Recipe, error)
 	Update(ctx context.Context, recipe Recipe) error
 	Delete(ctx context.Context, id string) error
 }

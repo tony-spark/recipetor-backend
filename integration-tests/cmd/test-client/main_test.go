71861f11b908b2a7a72668a75124c4c5d7361a9b
Anton Storozhev <i.am@tonysparked.com>
2022-12-22 03:49:01 +0300

    recipe-service: move kafka controller test to recipe-service

--- integration-tests/cmd/test-client/main_test.go
+++ integration-tests/cmd/test-client/main_test.go
@@ -3,14 +3,8 @@ package main
 import (
 	"os"
 	"testing"
-
-	"github.com/stretchr/testify/suite"
 )
 
 func TestMain(m *testing.M) {
 	os.Exit(m.Run())
-}
-
-func TestRecipeService(t *testing.T) {
-	suite.Run(t, new(RecipeServiceSuite))
 }
